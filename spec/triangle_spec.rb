f735059e5d62cf5dc374c8d6fc982054d711aba5
Hiroyuki Fukuda <fukuishere@gmail.com>
2017-12-07 20:39:16 +0900

    add: 二等辺三角形の判定条件を追加

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -6,49 +6,74 @@ describe Triangle do
   describe '#kind_of' do
     context '辺a が 0 以下の数値の場合' do
       it '三角形ではないことを返却すること' do
-        expect(Triangle.kind_of(0, 2, 2)).to eq '三角形ではありません > <'
-        expect(Triangle.kind_of(-0.1, 2, 2)).to eq '三角形ではありません > <'
-        expect(Triangle.kind_of(-3, 2, 2)).to eq '三角形ではありません > <'
+        message = '三角形ではありません > <'
+        expect(Triangle.kind_of(0, 2, 2)).to eq message
+        expect(Triangle.kind_of(-0.1, 2, 2)).to eq message
+        expect(Triangle.kind_of(-3, 2, 2)).to eq message
       end
     end
 
     context '辺b が 0 以下の数値の場合' do
       it '三角形ではないことを返却すること' do
-        expect(Triangle.kind_of(2, 0, 2)).to eq '三角形ではありません > <'
-        expect(Triangle.kind_of(2, -0.1, 2)).to eq '三角形ではありません > <'
-        expect(Triangle.kind_of(2, -3, 2)).to eq '三角形ではありません > <'
+        message = '三角形ではありません > <'
+        expect(Triangle.kind_of(2, 0, 2)).to eq message
+        expect(Triangle.kind_of(2, -0.1, 2)).to eq message
+        expect(Triangle.kind_of(2, -3, 2)).to eq message
       end
     end
 
     context '辺c が 0 以下の数値の場合' do
       it '三角形ではないことを返却すること' do
-        expect(Triangle.kind_of(2, 2, 0)).to eq '三角形ではありません > <'
-        expect(Triangle.kind_of(2, 2, -0.1)).to eq '三角形ではありません > <'
-        expect(Triangle.kind_of(2, 2, -3)).to eq '三角形ではありません > <'
+        message = '三角形ではありません > <'
+        expect(Triangle.kind_of(2, 2, 0)).to eq message
+        expect(Triangle.kind_of(2, 2, -0.1)).to eq message
+        expect(Triangle.kind_of(2, 2, -3)).to eq message
       end
     end
 
     context '三角形成立条件を満たさない場合' do
       it '三角形ではないことを返却すること' do
+        message = '三角形ではありません > <'
         # 辺a+b=c の場合
-        expect(Triangle.kind_of(1, 1, 2)).to eq '三角形ではありません > <'
+        expect(Triangle.kind_of(1, 1, 2)).to eq message
         # 辺a+b<c の場合
-        expect(Triangle.kind_of(1, 1, 3)).to eq '三角形ではありません > <'
+        expect(Triangle.kind_of(1, 1, 3)).to eq message
         # 辺b+c=a の場合
-        expect(Triangle.kind_of(2, 1, 1)).to eq '三角形ではありません > <'
+        expect(Triangle.kind_of(2, 1, 1)).to eq message
         # 辺b+c<a の場合
-        expect(Triangle.kind_of(3, 1, 1)).to eq '三角形ではありません > <'
+        expect(Triangle.kind_of(3, 1, 1)).to eq message
         # 辺c+a=b の場合
-        expect(Triangle.kind_of(1, 2, 1)).to eq '三角形ではありません > <'
+        expect(Triangle.kind_of(1, 2, 1)).to eq message
         # 辺c+a<b の場合
-        expect(Triangle.kind_of(1, 3, 1)).to eq '三角形ではありません > <'
+        expect(Triangle.kind_of(1, 3, 1)).to eq message
       end
     end
 
-    context '正三角形の成立条件を満たす場合' do
+    context '三角形の成立条件を満たす場合' do
       it '正三角形であることを返却すること' do
-        expect(Triangle.kind_of(0.1, 0.1, 0.1)).to eq '正三角形です'
-        expect(Triangle.kind_of(2, 2, 2)).to eq '正三角形です'
+        message = '正三角形です'
+        expect(Triangle.kind_of(0.1, 0.1, 0.1)).to eq message
+        expect(Triangle.kind_of(2, 2, 2)).to eq message
+      end
+
+      it '二等辺三角形であることを返却すること' do
+        message = '二等辺三角形です'
+        expect(Triangle.kind_of(0.1, 0.1, 0.01)).to eq message
+        expect(Triangle.kind_of(2, 2, 3)).to eq message
+        expect(Triangle.kind_of(0.01, 0.1, 0.1)).to eq message
+        expect(Triangle.kind_of(3, 2, 2)).to eq message
+        expect(Triangle.kind_of(0.1, 0.01, 0.1)).to eq message
+        expect(Triangle.kind_of(2, 3, 2)).to eq message
+      end
+
+      it '不等辺三角形であることを返却すること' do
+        message = '不等辺三角形です'
+        expect(Triangle.kind_of(0.1, 0.29, 0.3)).to eq message
+        expect(Triangle.kind_of(2, 3, 4)).to eq message
+        expect(Triangle.kind_of(0.3, 0.1, 0.29)).to eq message
+        expect(Triangle.kind_of(4, 2, 3)).to eq message
+        expect(Triangle.kind_of(0.29, 0.3, 0.1)).to eq message
+        expect(Triangle.kind_of(3, 4, 2)).to eq message
       end
     end
   end
--- triangle.rb
+++ triangle.rb
@@ -2,59 +2,47 @@
 
 # Triangle Class
 class Triangle
-  class << self
-    # 三角形の種類を返却
-    #
-    # @param [String] side_a 辺A
-    # @param [String] side_b 辺B
-    # @param [String] side_c 辺C
-    # @return [String] 三角形の種類のメッセージ
-    #
-    def kind_of(side_a, side_b, side_c)
-      side_a2f = side_a.to_f
-      side_b2f = side_b.to_f
-      side_c2f = side_c.to_f
-
-      return '三角形ではありません > <' unless valid?(side_a2f, side_b2f, side_c2f)
-      return '正三角形です' if regular?(side_a2f, side_b2f, side_c2f)
-    end
-
-    private
-
-    # バリデーション
-    #
-    # 以下の条件を満たすこと
-    # - 各辺が 0 より大きい
-    # - |b-c| < a < b+c
-    #   - a+b > c
-    #   - b+c > a
-    #   - c+a > b
-    #
-    # @param [Float] side_a2f 辺A
-    # @param [Float] side_b2f 辺B
-    # @param [Float] side_c2f 辺C
-    # @return [Boolean] バリデーション結果
-    #
-    def valid?(side_a2f, side_b2f, side_c2f)
-      side_a2f.positive? && side_b2f.positive? && side_c2f.positive? \
-        && side_a2f + side_b2f > side_c2f \
-        && side_b2f + side_c2f > side_a2f \
-        && side_c2f + side_a2f > side_b2f
-    end
-
-    # 正三角形判定
-    #
-    # 与えられた辺から正三角形か判定
-    #
-    # @param [Float] side_a2f 辺A
-    # @param [Float] side_b2f 辺B
-    # @param [Float] side_c2f 辺C
-    # @return [Boolean] 判定結果
-    #
-    def regular?(side_a2f, side_b2f, side_c2f)
-      [side_a2f, side_b2f, side_c2f].uniq.size == 1
-    end
+  # 三角形の種類を返却
+  #
+  # @param [String] side_a 辺A
+  # @param [String] side_b 辺B
+  # @param [String] side_c 辺C
+  # @return [String] 三角形の種類のメッセージ
+  #
+  def self.kind_of(side_a, side_b, side_c)
+    side_a2f = side_a.to_f
+    side_b2f = side_b.to_f
+    side_c2f = side_c.to_f
+    return '三角形ではありません > <' unless valid?(side_a2f, side_b2f, side_c2f)
+    return '正三角形です' if [side_a2f, side_b2f, side_c2f].uniq.size == 1
+    return '二等辺三角形です' if side_a2f == side_b2f \
+      || side_b2f == side_c2f \
+      || side_c2f == side_a2f
+    '不等辺三角形です'
   end
+
+  # バリデーション
+  #
+  # 以下の条件を満たすこと
+  # - 各辺が 0 より大きい
+  # - |b-c| < a < b+c
+  #   - a+b > c
+  #   - b+c > a
+  #   - c+a > b
+  #
+  # @param [Float] side_a2f 辺A
+  # @param [Float] side_b2f 辺B
+  # @param [Float] side_c2f 辺C
+  # @return [Boolean] バリデーション結果
+  #
+  def self.valid?(side_a2f, side_b2f, side_c2f)
+    side_a2f.positive? && side_b2f.positive? && side_c2f.positive? \
+      && side_a2f + side_b2f > side_c2f \
+      && side_b2f + side_c2f > side_a2f \
+      && side_c2f + side_a2f > side_b2f
+  end
+
+  private_class_method :valid?
 end
 
 p Triangle.kind_of(ARGV[0], ARGV[1], ARGV[2])
