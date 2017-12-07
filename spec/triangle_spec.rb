9b5491a8ff18128b9a8b4d031e5d57e4e0e769e3
Hiroyuki Fukuda <fukuishere@gmail.com>
2017-12-07 19:36:50 +0900

    add: 正三角形の判定条件を追加

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -6,43 +6,49 @@ describe Triangle do
   describe '#kind_of' do
     context '辺a が 0 以下の数値の場合' do
       it '三角形ではないことを返却すること' do
-        pending '0 の場合'
-        pending '-0.1 の場合'
-        pending '-3 の場合'
+        expect(Triangle.kind_of(0, 2, 2)).to eq '三角形ではありません > <'
+        expect(Triangle.kind_of(-0.1, 2, 2)).to eq '三角形ではありません > <'
+        expect(Triangle.kind_of(-3, 2, 2)).to eq '三角形ではありません > <'
       end
     end
 
     context '辺b が 0 以下の数値の場合' do
       it '三角形ではないことを返却すること' do
-        pending '0 の場合'
-        pending '-0.1 の場合'
-        pending '-3 の場合'
+        expect(Triangle.kind_of(2, 0, 2)).to eq '三角形ではありません > <'
+        expect(Triangle.kind_of(2, -0.1, 2)).to eq '三角形ではありません > <'
+        expect(Triangle.kind_of(2, -3, 2)).to eq '三角形ではありません > <'
       end
     end
 
     context '辺c が 0 以下の数値の場合' do
       it '三角形ではないことを返却すること' do
-        pending '0 の場合'
-        pending '-0.1 の場合'
-        pending '-3 の場合'
+        expect(Triangle.kind_of(2, 2, 0)).to eq '三角形ではありません > <'
+        expect(Triangle.kind_of(2, 2, -0.1)).to eq '三角形ではありません > <'
+        expect(Triangle.kind_of(2, 2, -3)).to eq '三角形ではありません > <'
       end
     end
 
     context '三角形成立条件を満たさない場合' do
       it '三角形ではないことを返却すること' do
-        pending '辺a+b=c の場合'
-        pending '辺a+b<c の場合'
-        pending '辺b+c=a の場合'
-        pending '辺b+c<a の場合'
-        pending '辺c+a=b の場合'
-        pending '辺c+a<b の場合'
+        # 辺a+b=c の場合
+        expect(Triangle.kind_of(1, 1, 2)).to eq '三角形ではありません > <'
+        # 辺a+b<c の場合
+        expect(Triangle.kind_of(1, 1, 3)).to eq '三角形ではありません > <'
+        # 辺b+c=a の場合
+        expect(Triangle.kind_of(2, 1, 1)).to eq '三角形ではありません > <'
+        # 辺b+c<a の場合
+        expect(Triangle.kind_of(3, 1, 1)).to eq '三角形ではありません > <'
+        # 辺c+a=b の場合
+        expect(Triangle.kind_of(1, 2, 1)).to eq '三角形ではありません > <'
+        # 辺c+a<b の場合
+        expect(Triangle.kind_of(1, 3, 1)).to eq '三角形ではありません > <'
       end
     end
 
     context '正三角形の成立条件を満たす場合' do
       it '正三角形であることを返却すること' do
-        pending '辺a=0.1, 辺b=0.1, 辺c=0.1 の場合'
-        pending '辺a=2,   辺b=2,   辺c=2 の場合'
+        expect(Triangle.kind_of(0.1, 0.1, 0.1)).to eq '正三角形です'
+        expect(Triangle.kind_of(2, 2, 2)).to eq '正三角形です'
       end
     end
   end
